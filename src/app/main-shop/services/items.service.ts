a396856a43ba39c517c1ec92e27b0ba9f8aec6da
faizal kamaruddin <faizal.itech92@gmail.com>
2021-08-08 21:40:53 +0800

    some changes

--- src/app/main-shop/services/items.service.ts
+++ src/app/main-shop/services/items.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { environment } from '@src/environments/environment'
 import { IProducts } from '../interfaces/products.interface';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 
 @Injectable({
@@ -19,9 +19,16 @@ export class ItemsService {
   }
   
   getProducts(productId = '', filename = ''): Observable<IProducts[]> {
-    if (Boolean(productId) && Boolean(filename))
+    if (Boolean(productId) && Boolean(filename)){
+      console.log(productId, filename)
       this.apiUrl = `${this.apiUrl}/${productId}/${filename}`
+      return of([])
+    }
     
     return this._http.get<IProducts[]>(`${this.apiUrl}`, { 'headers': this.headers })
+  }
+  
+  getimages() {
+    return this._http.get('http://149.28.132.97:7000/api/products/60c92e55e31bc648306a8483/001-1.jpg', { 'headers': this.headers })
   }
 }
