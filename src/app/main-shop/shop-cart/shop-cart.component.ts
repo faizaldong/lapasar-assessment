d86669bde88d1009dd81b006994fb467f5e952ca
faizal kamaruddin <faizal.itech92@gmail.com>
2021-08-08 18:07:32 +0800

    prepared UI for cart page

--- src/app/main-shop/shop-cart/shop-cart.component.ts
+++ src/app/main-shop/shop-cart/shop-cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute, Router } from '@angular/router'
+import { IProducts } from '../interfaces/products.interface'
+import * as _ from 'lodash'
 
 @Component({
     selector: 'app-shop-cart',
@@ -7,7 +10,50 @@ import { Component, OnInit } from '@angular/core'
 })
 
 export class ShopCartComponent implements OnInit {
+    listCarts: IProducts[] = []
+
+    constructor(
+        private router: Router
+    ) {}
+
     ngOnInit() {
-        console.log('ONINIT CART')
+        this.getAllProductCart()
+    }
+
+    getAllProductCart() {
+        const carts = localStorage.getItem('carts')
+        if (this.getCartsStored.length) {
+            this.listCarts = this.getCartsStored
+            console.log('after uniq', this.listCarts)
+        } else {
+            this.router.navigate(['/list'])
+        }
+    }
+    
+    convertCentToRM(price: number) {
+        return (price * 0.004163).toFixed(2);
+    }
+
+    productPrice(item: IProducts) {
+        return item.cartTotal * +this.convertCentToRM(item.price)
+    }
+
+    setItemTotal(item: IProducts, event: string) {
+        if (event === 'dec') {
+            if (item.cartTotal == 0)
+                return
+            item.cartTotal -= 1
+        } else {
+            item.cartTotal += 1
+        }
+    }
+
+    get getCartsStored(): IProducts[] {
+        let localCarts = localStorage.getItem('carts')
+        if (localCarts && JSON.parse(localCarts).length) {
+            return JSON.parse(localCarts)
+        } else {
+            return []
+        }
     }
 }
